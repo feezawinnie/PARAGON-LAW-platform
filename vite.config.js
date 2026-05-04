d58abac253c8a89d2b0038fad33f30eba36d7244
feezawinnie <feezawinnie5@gmail.com>
2026-05-04 09:19:47 +0200

    revised version of paragon-firm

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+        contact: resolve(__dirname, "contact.html"),
+      },
+    },
+  },
 });
